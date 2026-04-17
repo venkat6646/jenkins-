bfe0ea0d929025761e4adc55b72504aee766dda7
venkat6646 <aryababy6646@gmail.com>
2026-04-17 17:05:10 +0530

    Webhook testing

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello
 	public static void main(String ...args)
 	{
 		System.out.println("Hello from Java");
+		System.out.println("Web hook added in github with the help of ngrok server");
 	}
 }
 
