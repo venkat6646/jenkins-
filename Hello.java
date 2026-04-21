c94d75751e0aed9578ad15c91885a612ec830b26
venkat6646 <aryababy6646@gmail.com>
2026-04-21 20:47:07 +0530

    Pipeline added with webhook

--- Hello.java
+++ Hello.java
@@ -3,7 +3,11 @@ public class Hello
 	public static void main(String ...args)
 	{
 		System.out.println("Hello from Java");
+
 		System.out.println("Web hook added in github with the help of ngrok server");
+		
+		System.out.println("Pipeline added");
+
 	}
 }
 
